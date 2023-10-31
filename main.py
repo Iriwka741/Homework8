1a4283e40f60ed424a4d2a34727a43f907d5f753
Lenovo <katashirina89@gmail.com>
2023-10-31 19:06:34 +0300

    Implemented first task

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("Hello teacher")
-print("Homework №8 Banul")
+#print("Hello teacher")
+#print("Homework №8 Banul")
+
+#Написати валідації за допомогою регулярних виразів:
+#1.  домашній номер телефону (тільки цифри та довжина номера)
+
+import re
+def valid_home_number(number):
+    pattern = re.compile(r'^\d{10}$')
+    return bool(pattern.match(number))
+
+home_number = "1234567"
+print(valid_home_number(home_number))
